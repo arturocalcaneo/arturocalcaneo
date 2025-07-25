81250f2acbe36c3e4a0e6057e0d2301c242a6a96
Arturo Alejandro Calcaneo Badillo <aa.calcaneob@gmail.com>
2025-07-24 19:53:49 -0600

    Portafolio arturocalcaneo v1.2

--- js/index.js
+++ js/index.js
@@ -50,6 +50,30 @@ $(document).ready(function(){
                 }
             }
 
+            // Al recargar la página, al hacer scroll verficiar si el texto del navbar ya está en color negro y si el body contiene la clase not-on-top.
+            var windowScrollContinuar= true;
+            $(window).on('scroll', function(){
+                if(windowScrollContinuar){
+                    const body= document.getElementsByTagName('body')[0];
+                    const navbarBrand= document.getElementsByClassName('navbar-brand')[0];
+                    const navbarNav= document.getElementsByClassName('navbar-nav')[0];
+
+                    if(body.classList.contains('not-on-top')){
+                        if(navbarBrand.style.color == 'white'){
+                            document.getElementsByClassName('navbar-brand')[0].style.color= 'black';
+                        }
+
+                        for(let li of navbarNav.querySelectorAll('.nav-item')){
+                            if (li.querySelector('a').style.color === 'white') {
+                                li.querySelector('a').style.color = 'black';
+                            }
+                        }
+
+                        windowScrollContinuar= false; // Detener el scroll para evitar reasignación del mismo color.
+                    }
+                }
+            });
+
         // CASO 2: Por cambios en las clases (navbar).
 
             // Observador para detectar cambios en las clases del navbar
@@ -75,10 +99,9 @@ $(document).ready(function(){
 
             // Detectar el cambio de tamaño de la ventana
             $(window).resize(function(){
-                if(window.innerWidth < 992)
+                if(document.getElementsByTagName('body')[0].classList.contains('not-on-top')){
                     navbarColors('black');
-                else
-                    navbarColors('white');
+                }
 
                 // Actualizar el texto del elemento medio en la sección del "contador"
                 if(window.innerWidth >= 768 && window.innerWidth < 992) {
